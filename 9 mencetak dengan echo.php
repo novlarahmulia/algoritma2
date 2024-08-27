775b367cde733b4b92eb8dfd9a2e8d133b9ecff6
novlarahmulia <rahmulianovla@gmail.com>
2024-08-27 22:04:49 +0700

    menampilkan nilai dari variabel php

---
+++ 9 mencetak dengan echo.php
@@ -0,0 +1,14 @@
+<?php
+
+
+// membuat variabel baru
+$nama_barang = "Minyak wangi";
+$harga = 15000;
+$tempat = "simpang";
+// menampilkan isi variabel
+echo "novla membeli $nama_barang seharga Rp $harga di$tempat";
+?>
+
+
+
+
