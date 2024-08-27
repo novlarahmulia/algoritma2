0df65f1e6466976d4b7177466f95cf078299b9cc
novlarahmulia <rahmulianovla@gmail.com>
2024-08-27 21:23:04 +0700

    membuat variabel dalam php

---
+++ 7 membuat variabel dalam php.php
@@ -0,0 +1,52 @@
+<?php
+
+$nama = "novla";
+$harga = 1000;
+$nama_barang = "baju wanita";
+$harga = 5000;
+$stok = 40;
+// membuat variabel baru
+$stok = 40;
+
+// mengisi ulang variabel dengan nilai baru
+$stok = 34;
+
+// 1)	Awal dari nama variabel tidak boleh menggunakan angka dan simbol, kecuali underscore.
+// Contoh: Benar
+$namakepanjangan = "novla rahmulia";
+$namapendek = "novla";
+// 2)	Nama variabel yang terdiri dari dua suku kata, bisa dipisah dengan underscore (_) atau menggunakan style camelCase.
+// Contoh:
+$namabArang = "sandal wanita";
+$namaPembeli = "nayla"; // menggunakan camelCase
+
+// 3)	Variabel harus diisi saat pembuatannya. Bila kita tidak ingin mengisi, cukup isi dengan nilai kosong.
+// Contoh:
+$nama_barang = "kalung wanita";
+$namaPembeli = "nayla";
+$harga = 5000;
+
+// 4)	Nama variabel bersifat Case Sensitive, artinya huruf besar dan huruf kecil dibedakan.
+// Contoh: Tiga variabel ini akan dianggap berbeda.
+$Belajar = "PHP";
+$BELAJAR = "Html";
+$belajar = "Css";
+
+// =================== cara mencetak =======================
+echo 
+$nama,"<p>".
+$harga,"<p>".
+$nama_barang,"<p>".
+$nama_barang,"<p>".
+$harga,"<p>".
+$stok,"<p>".
+$nama,"<p>".
+$namapendek,"<p>".
+$nama_barang,"<p>".
+$namaPembeli,"<p>". 
+$nama_barang,"<p>".
+$namaPembeli,"<p>".
+$harga,"<p>".
+$Belajar,"<p>".
+$BELAJAR,"<p>".
+$belajar;
