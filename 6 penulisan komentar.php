531d8b5a7362ec3f8dfe7193e4f7890efd2558de
novlarahmulia <rahmulianovla@gmail.com>
2024-08-27 13:05:23 +0700

    membuat komentar di php

---
+++ 6 penulisan komentar.php
@@ -0,0 +1,16 @@
+<?php 
+
+// ini adalah komentar
+echo "Hello world";
+
+/*
+ini adalah komentar 
+yang lebih dari satu 
+baris
+*/
+echo "<br>saya sudah hampir dewasa";
+echo "<br>saya sudah boleh minum obat";
+echo "<br>saya sudah boleh minum obat";
+echo "<br>Nama : Novla Rahmulia";
+
+?>
