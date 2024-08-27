b2c34073075345077c128b3164c7739a4e0813b5
novlarahmulia <rahmulianovla@gmail.com>
2024-08-27 10:55:29 +0700

    membuat statement dan ekspresi

---
+++ 4 menulis statement dan ekspresi.php
@@ -0,0 +1,21 @@
+<?php
+echo "ini statement 1<p>";
+echo "ini statement 2<p>";
+//deklarasikan sebuah variable
+$b=10;
+$c=6;
+//ekspresi
+$a1 = $b * $c;
+$a2 = $b + $c;
+//cetak ekspresi
+echo "<p>ini expresi -> $a1 = $b * $c <p>";
+echo "<p>ini expresi -> $a2 = $b + $c <p>";
+echo "<p>yaitu : ".$a1;
+echo "<p>yaitu : ".$a2;
+echo "<p>by Novla Rahmulia</p>";
+
+
+echo "ini statement 1";
+echo "ini statement 2";
+$a = $b + $c;
+?>
