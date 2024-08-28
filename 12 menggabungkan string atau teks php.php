f99429a6318cbdbcec62e218bbe3b7565d438b4c
novlarahmulia <rahmulianovla@gmail.com>
2024-08-28 08:20:45 +0700

    menggabungkan string di php

---
+++ 12 menggabungkan string atau teks php.php
@@ -0,0 +1,10 @@
+<?php
+$txt1 = "Tutorial PHP";
+$txt2 = "novlarahmulia";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Dibelajar pemrograman PHP di " . $txt2 . "<br>";
+echo $x + $y;
+?>
